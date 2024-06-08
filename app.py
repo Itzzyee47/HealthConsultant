e43dd94831d705b1ceb4af163bfd8e5b5ca77859
ItzZyee47 <ebongloveis@gmail.com>
2024-06-08 00:16:23 -0700

    redeifined the apis

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from model import getResponds
 
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import db, auth
+from firebase_admin import db
 
 # Replace with your Firebase project configuration
 cred = credentials.Certificate('healthKey.json')  # Download from Firebase console
@@ -29,20 +29,21 @@ def login_required(func):
     return wrapper_func
 
 # send message to database...
-def send_message(chat_room_id, sender_id, message):
-  """Sends a message to a specific chat room, creating it if it doesn't exist."""
-  chat_ref = ref.child('chats').child(chat_room_id)
-  
-  # Check if chat room exists
-  if not chat_ref.get():
-    chat_ref.set({})  # Create an empty chat room object with uniuqe id
-    
-  message_ref = chat_ref.child('messages').push()  # Push message
-  message_ref.set({
-    'content': message,
-    'sender': sender_id,
-    'timestamp': datetime.now().timestamp()  # Server-side timestamp
-})
+def send_message(chat_room_id, sender_id, message, response):
+  """Sends a message to a specific chat room, which must exist."""
+  try:
+    chat_ref = ref.child('messages')
+    chat_ref.set({})
+    message_ref = chat_ref.push()  # Push message
+    message_ref.set({
+      'chat': chat_room_id,
+      'question': message,
+      'response':response,
+      'sender': sender_id,
+      'timestamp': datetime.now().timestamp()  # Server-side timestamp
+    })
+  except :
+    return KeyError
     # convert the timestamp to a datetime object in the local timezone
     # dt_object = datetime.fromtimestamp(timestamp)
     
@@ -55,42 +56,77 @@ def on_message(changes):
   """Listener function to handle new messages."""
   print(f"New message: {changes}" )
     
-    
-# Example usage
-# chat_room_id = 'chatRoom1'
-# sender_id = 'user123'
-# message = 'Hello, everyone!'
 
-# send_message(chat_room_id, sender_id, message)
-
-# def on_message(snapshot, changes):
-  # """Listener function to handle new messages."""
-  # for change in changes:
-    # print(f"New message: {change.val()['content']}")
-
-# get_messages(chat_room_id, on_message)
-  
-
-@app.route("/", methods=['POST'])
-def index():
+@app.route("/getResponse", methods=['POST'])
+def converse():
   # Get the user input from the request
+  convoID = request.form["convoID"]
   data = request.form["message"]
   user = request.form['user']
   print(data)
-  # Send to the model and get it responds
-  response = getResponds(data)
-  # save user sent message 
-  send_message(user, user, data) 
-  
-  get_messages(user, on_message(response))
-  # save bot sent message 
-  send_message(user, 'bot', response)
-  
-  message = {"answer": response}
-  
-  return jsonify(message)
+  try:
+    # Send to the model and get it responds
+    response = getResponds(data)
+    # save user sent message 
+    send_message(convoID, user, data, response) 
+    
+    message = {"answer": response}
+    
+    return jsonify(message)
+  except Exception as e:
+    return f'An error occured: {e}', 500
 
+@app.route("/createConvo", methods=['POST'])
+def create():
+  # Get the user input from the request
+  user = request.form['user']
+  chat_ref = ref.child('chats')
+  # error handelling
+  try: 
+    chat_ref.set({})  # Create an empty chat room object with uniuqe id
+    convoRef = chat_ref.push()  # Push creation data by who and when
+    convoRef.set({
+      'sender': user,
+      'time': datetime.now().timestamp()  # Server-side timestamp
+    })
+    
+    message = {"chatID": chat_ref}
+    
+    return jsonify(message)
+  except Exception as e:
+    return 500
+  
+@app.route("/getConversations", methods=['POST'])
+def getConvos():
+  # Get the user input from the request
+  user = request.form['user']
+  chat_ref = ref.child('chats')
+  # error handelling
+  try: 
+    # Get all coversations belonging to a specofic user....
+    convos = chat_ref.order_by_child('user').equal_to(user).get()  
 
+    message = {"convos": convos}
+    
+    return jsonify(message)
+  except Exception as e:
+    return f'An error occured: {e}', 500
+  
+@app.route("/getMessagesOfChat", methods=['POST'])
+def getConvos():
+  # Get the user input from the request
+  convoID = request.form["convoID"]
+  mess_ref = ref.child('messages')
+  # error handelling
+  try: 
+    # Get all messages belonging to a specific conversation
+    convos = mess_ref.order_by_child('chat').equal_to(convoID).get()  
+
+    message = {"messages": convos}
+    
+    return jsonify(message)
+  except Exception as e:
+    return f'An error occured: {e}', 500
 
 
 if __name__ == "__main__":
